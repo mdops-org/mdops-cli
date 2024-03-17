93e86ad03f7730fe85ec8fbeb081ddb158b0f685
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2024-03-17 02:35:01 +0100

    fix: make mdops.sh more portable

--- mdops.sh
+++ mdops.sh
@@ -13,10 +13,10 @@ while [ "$currentDir" != "/" ]; do
     currentDir=$(dirname "$currentDir")
 done
 
-cmd="sh <(curl -Ssf https://pkgx.sh) deno@1.41.2 run -A --unstable-ffi --unstable-fs https://raw.githubusercontent.com/mdops-org/mdops-cli/main/main.ts"
-
 if $found; then
-    cmd="$currentDir/scripts/mdops"
-fi
+    $currentDir/scripts/mdops "$@"
+else
+    eval "$(curl -Ssf https://pkgx.sh)"
 
-$cmd "$@"
+    pkgx deno@1.41.2 run -A --unstable-ffi --unstable-fs https://raw.githubusercontent.com/mdops-org/mdops-cli/main/main.ts "$@"
+fi
